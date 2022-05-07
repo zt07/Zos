d76d8125e4a2e468eaf38bc4db9a17fff335544a
zt07 <zaheeb07@gmail.com>
2022-05-07 10:30:32 -0400

    literally from barebones on osdev but a start ig

--- kernel.cpp
+++
@@ -1,8 +0,0 @@
-// Writes 'f' to the console.
-//c compilation isnt working so for now im writing c code in a cpp file
-
-extern "C" void main(){
-    *(char*)0xb8000 = 'f';
-    
-    return;
-}
--- zeroes.asm
+++
@@ -1,2 +0,0 @@
-;generates a bin file full of zeros, prevents overwriting in the disk
-times 10240 db 0
