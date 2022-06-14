4c459bfce166bf4a8209e2f8bc6d0c41ccd11e07
zt07 <zaheeb07@gmail.com>
2022-06-14 18:06:50 -0400

    refactor, object files delete after the linking in th ebuild script

--- boot.sh
+++ boot.sh
@@ -1,10 +1,2 @@
-mkdir -p isodir/boot/grub
-cp myos.bin isodir/boot/myos.bin
-cp grub.cfg isodir/boot/grub/grub.cfg
-grub-mkrescue -o myos.iso isodir
-if grub-file --is-x86-multiboot myos.bin; then
-  echo multiboot confirmed
-else
-  echo the file is not multiboot
-fi
+
 qemu-system-i386 -cdrom Zos.iso
--- build.sh
+++ build.sh
@@ -4,11 +4,11 @@ i686-elf-as boot.s -o boot.o
 i686-elf-gcc -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
 i686-elf-gcc -T linker.ld -o Zos.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc
 rm boot.o kernel.o
-cp myos.bin isodir/boot/myos.bin
+cp Zos.bin isodir/boot/Zos.bin
 cp grub.cfg isodir/boot/grub/grub.cfg
 
 grub-mkrescue -o Zos.iso isodir
-if grub-file --is-x86-multiboot myos.bin; then
+if grub-file --is-x86-multiboot Zos.bin; then
   echo multiboot confirmed
 else
   echo the file is not multiboot
